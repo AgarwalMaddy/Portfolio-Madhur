4787f7e24d04ce84864980d042ed244a86d3df29
Madhur Agarwal <madhur1812agarwal@gmail.com>
2023-11-26 10:37:22 +0530

    Changed the footer text :memo:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Madhur Agarwal</h3>
+          <h3>Madhur Agarwal</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} MA</h3>
