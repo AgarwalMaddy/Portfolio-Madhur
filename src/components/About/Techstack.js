7974dd377879ffbbee9d9534fea3117ea1b1e1c2
Madhur Agarwal <madhur1812agarwal@gmail.com>
2023-12-20 11:53:13 +0530

    Tech Stack Order

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -14,20 +14,22 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiJava />
           </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMysql />
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      
+      
     </Row>
   );
 }
